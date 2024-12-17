cf856d560db16bec27331d34e7158c896998ae1d
Mahdi Hasan <mahdi.hasan@vivacomsolutions.com>
2024-12-17 13:14:44 +0600

    Out put in KB

--- ImageCompressor.cs
+++ ImageCompressor.cs
@@ -159,16 +159,17 @@ public class ImageCompressor
         var lines = new List<string>
             {
                 // Header
-                "FileName\tLibrary\tTargetSize\tFormat\tCompressionTime(ms)\tMemoryUsed(bytes)\tInputFileSize(bytes)\tOutputFileSize(bytes)"
+                "FileName\tLibrary\tFormat\tCompressionTime(ms)\tMemoryUsed(KB)\tInputFileSize(KB)\tOutputFileSize(KB)"
             };
 
-        // Add data rows
+        // Add data rows with sizes in KB
         lines.AddRange(results.Select(r =>
-            $"{r.FileName}\t{r.LibraryName}\t{r.TargetSize}\t{r.Format}\t{r.CompressionTimeMs}\t{r.MemoryUsedBytes}\t{r.InputFileSizeBytes}\t{r.OutputFileSizeBytes}"
+            $"{r.FileName}\t{r.LibraryName}\t{r.Format}\t{r.CompressionTimeMs}\t{r.MemoryUsedBytes / 1024.0:F2}\t{r.InputFileSizeBytes / 1024.0:F2}\t{r.OutputFileSizeBytes / 1024.0:F2}"
         ));
 
+
         // Write to file
-        var fileName = $"compression_report_{DateTime.Now:yyyy-MM-dd-HH-mm}.tsv";
+        var fileName = $"compression_report_{DateTime.Now:dd-MMM_HH-mm-G}.tsv";
         File.WriteAllLines(Path.Combine(outputPath,fileName), lines);
         Console.WriteLine($"Report generated: {outputPath}");
     }
